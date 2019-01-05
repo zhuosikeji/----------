515b58e458e7b9aab9eac36087896f2d4cc62d8b
kaijiangkulianxintai <41614629+15271806447@users.noreply.github.com>
2019-01-05 14:34:32 +0800

    积分

--- 同源梦小程序项目/pages/integral/integral.js
+++ 同源梦小程序项目/pages/integral/integral.js
@@ -1,3 +1,5 @@
+var app=getApp();
+
 Page({
 
   /**
@@ -5,49 +7,101 @@ Page({
    */
   data: {
     user:{
-      nickname:"dqq",
-      jifenTotal:12530,
+      nickname:"",
+      jifenTotal:0,
       jifenDetail:[
-        {
-          jifenName:"消费赚积分",
-          jifenType:"收入",
-          jifenNumber:27,
-          jifenDate:"2019/01/03 21:22"
-        },
-        {
-          jifenName: "消费赚积分",
-          jifenType: "收入",
-          jifenNumber: 27,
-          jifenDate: "2019/01/03 21:22"
-        },
-        {
-          jifenName: "消费赚积分",
-          jifenType: "收入",
-          jifenNumber: 27,
-          jifenDate: "2019/01/03 21:22"
-        },
-        {
-          jifenName: "消费赚积分",
-          jifenType: "收入",
-          jifenNumber: 27,
-          jifenDate: "2019/01/03 21:22"
-        },
-        {
-          jifenName: "消费赚积分",
-          jifenType: "收入",
-          jifenNumber: 27,
-          jifenDate: "2019/01/03 21:22"
-        },
       ]
-    }
+    },
     
   },
+
+  getIntegralList: function(){
+    var that = this;
+    var user = that.data.user;
+    wx:wx.request({
+      url: app.globalData.url + '/api/personalCenter/getIntegralRecord?userId=' + app.globalData.uid +'&sid=' + app.globalData.sid,
+      header: {
+        'X-Requested-With': 'APP'
+      },
+      method: 'POST',
+      success: function(res) {
+        console.log(res);
+      
+        var TotalList = res.data.data.hcIntegralRecordList;
+        var jifenList = new Array(TotalList.length);
+        
+        //获取nickname
+ 
+        var totalIntegral = 0;
+        //填写jifenDetail 积分详情
+        for (var i = 0; i < TotalList.length; i++) {
+          var jifen = {
+            jifenName: "",
+            jifenType: "",
+            jifenNumber: 0,
+            jifenDate: ""
+          };
+          // 积分类型判断
+          if(TotalList[i].type == 1){
+            jifen.jifenType = "收入";
+            jifen.jifenNumber = "+"+TotalList[i].integralQuantity;
+            totalIntegral += TotalList[i].integralQuantity;
+          }else if(TotalList[i].type == 2){
+            jifen.jifenType = "支出";
+            jifen.jifenNumber = "-" + TotalList[i].integralQuantity;
+            totalIntegral -= TotalList[i].integralQuantity;
+          }
+          // 积分来源类型
+          // 1 - 订单使用积分 2 - 订单结算得积分 3 - 订单取消返还 4 - 签到
+          if (TotalList[i].integralSourceType == 1){
+            jifen.jifenName = "订单使用积分";
+          } else if (TotalList[i].integralSourceType == 2){
+            jifen.jifenName = "订单结算得积分";
+          }else if (TotalList[i].integralSourceType == 3) {
+            jifen.jifenName = "订单取消返还";
+          }else if (TotalList[i].integralSourceType == 4) {
+            jifen.jifenName = "签到";
+          }
+          
+          //设置积分日期
+          jifen.jifenDate = that.dateSplite(TotalList[i].createTime);
+
+          jifenList[i] = jifen;
+        }
+        //设置总积分
+        user.jifenTotal = totalIntegral;
+        user.jifenDetail = jifenList;
+        
+        wx.getUserInfo({
+          success: function (res) {
+            user.nickname = res.userInfo.nickName;
+            console.log('nickName' + user.nickname)
+          }
+        })
+
+        that.setData({
+          'user': user
+        })
+        console.log('=====================');
+        console.log(that.data.user);
+
+      },
+    })
+  },
+
+  dateSplite:function(date){
+    date = date.substring(0,16);
+    return date;
+  },
+
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var that = this;
+    that.getIntegralList();
   },
 
   /**
